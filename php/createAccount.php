8577beb795742c4c39c16f6b4ae472f8f7195e71
Nathan Moore <moorednathan@gmail.com>
2013-10-07 10:48:18 -0500

    Made many minor changes to index.html and createAccount.php

--- php/createAccount.php
+++ php/createAccount.php
@@ -4,10 +4,29 @@
 	</head>
 	<body>
 		<?php
+			echo "Here I Am!";
 			//will need to connect to the database
+			// $con = mysql_connect("localhost","root","AtWsBcOotLFRR!");
+			// if (!con)
+			// {
+			// 	die('Could not connect: ' .mysql_error());
+			// }
 
+			// mysql_select_db("customcupcakes", $con) or die("Unable to select database:" .mysql_error());
+			// $query = 
 			//get form data
-			
+			$formData = array(); //an array to house the submitted from data
+
+			foreach($_POST as $key => $val)
+			{
+				$formData[$key] = htmlentities($val,ENT_QUOTES,'UTF-8');
+			}
+
+			if (isset($formData['telephone'])
+			{
+				//$expression = array('telephone')
+			}
+
 		?>
 	</body>
 </html>
