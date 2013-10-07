85aa23ec6e1f16cb6dae3f4651aa4b5ee15fd166
Nathan Moore <moorednathan@gmail.com>
2013-10-07 12:22:45 -0500

    I now store all the form input data in an array (in createAccount.php) and output the contents to the screen for debugging purposes

--- php/createAccount.php
+++ php/createAccount.php
@@ -8,13 +8,29 @@
 		<?php
 			
 			$formData = array(); //an array to house the submitted from data
+			//[0] = joinYes || joinNo
+			//[1] = firstName
+			//[2] = lastName
+			//[3] = email
+			//[4] = password
+			//[5] = telephone
+			//[6] = address
+			//[7] = city
+			//[8] = state
+			//[9] = zipcode
+
 
 			foreach($_POST as $key => $val)
 			{
 				$formData[$key] = htmlentities($val,ENT_QUOTES,'UTF-8');
+				if (isset($formData[$key]))
+				{
+					echo $key . ": " . $val . "&";
+				} else
+				{
+					echo 'false\n';
+				}
 			}
-
-			
 
 		?>
 	</body>
