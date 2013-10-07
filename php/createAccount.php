82c6a58d496f4eb8a4dc40e7a2a5d13b0a3070bd
Nathan Moore <moorednathan@gmail.com>
2013-10-07 12:03:47 -0500

    I linked index.html to createAccount.php and made all text associated with an input in a form (e.g., Join Mailing List: ) labels for better symantic definitions

--- php/createAccount.php
+++ php/createAccount.php
@@ -1,20 +1,12 @@
+<!-- Author: Nathan Moore -->
+
 <html>
 	<head>
 		<title>Create Account Form</title>
 	</head>
 	<body>
 		<?php
-			echo "Here I Am!";
-			//will need to connect to the database
-			// $con = mysql_connect("localhost","root","AtWsBcOotLFRR!");
-			// if (!con)
-			// {
-			// 	die('Could not connect: ' .mysql_error());
-			// }
-
-			// mysql_select_db("customcupcakes", $con) or die("Unable to select database:" .mysql_error());
-			// $query = 
-			//get form data
+			
 			$formData = array(); //an array to house the submitted from data
 
 			foreach($_POST as $key => $val)
@@ -22,10 +14,7 @@
 				$formData[$key] = htmlentities($val,ENT_QUOTES,'UTF-8');
 			}
 
-			if (isset($formData['telephone'])
-			{
-				//$expression = array('telephone')
-			}
+			
 
 		?>
 	</body>
