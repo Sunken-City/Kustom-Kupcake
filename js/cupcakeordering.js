2b7552274d08f66fc8c71ba9f9f076fd13af2db6
MissingNo <spacepirate92@gmail.com>
2013-10-09 14:11:20 -0500

    Added in some new javascript files, added a selection clause to css. Edited html slightly

---
+++ js/cupcakeordering.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+	function selectImage(e){
+		var pics=document.getElementsByTagName('img');
+
+		for(var i = 0, len=pics.length;i < len; i++) {
+			pics[i].setAttribute('class','');
+		}
+
+		var picture = e.target;
+		picture.setAttribute('class','selected)');
+	}
+
+	var imgs = document.getElementsByTagName('img');
+
+	for(var i = 0, len = imgs.length;i<len;i++) {
+		imgs[i].addEventListener('click',selectImage,false);
+	}
+};
---
+++ php/cupcakeOrdering.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title> Cupcake Ordering </title>
+	</head>
+	<body>
+		<?php
+			$con=mysql_connect("localhost","cupcaker","nomnomnom");
+
+			if (!$con){
+				die('Could not connect: ' . mysql_error());
+			}
+
+			mysql_select_db("")
+		?>
+	<body>
+</html>
