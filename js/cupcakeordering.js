3be8841e2bd26660db0235057d2199bc096cb8d3
MissingNo <spacepirate92@gmail.com>
2013-10-09 23:50:18 -0500

    Edited javascript

--- js/cupcakeordering.js
+++ js/cupcakeordering.js
@@ -1,22 +1,84 @@
 $(document).ready(function() {
 	alert("loaded");
-	function selectImage(e){
 
-		var testTable = $("#Flavor");
-		var pics=document.getElementsByClassName('FlavorClass');
+	var checkBoxesButton = document.getElementById('checkAll');
+	var uncheckBoxesButton = document.getElementById('uncheckAll');
 
-		for(var i = 0, len=pics.length;i < len; i++) {
-			pics[i].setAttribute('class','');
-			pics[i].setAttribute('class','FlavorClass');
+	uncheckBoxesButton.onclick=uncheckAllBoxes;
+	checkBoxesButton.onclick=checkAllBoxes;
+	function selectFlavor(e){
+
+		var flavors=document.getElementById('Flavor').getElementsByTagName('td');
+
+		for(var i = 0, len=flavors.length;i < len; i++) {
+			flavors[i].setAttribute('class','');
 		}
 
-		var picture = e.target;
-		picture.setAttribute('class','selected');
+		var selectedFlavor = e.target;
+		selectedFlavor.setAttribute('class','selected');
 	}
-	var testTable = $("#Flavor");
-	var imgs = document.getElementsByClassName('FlavorClass');
+	var flavors = document.getElementById('Flavor').getElementsByTagName('td');
 
-	for(var i = 0, len = imgs.length;i<len;i++) {
-		imgs[i].addEventListener('click',selectImage,false);
+	for(var i = 0, len = flavors.length;i<len;i++) {
+		flavors[i].addEventListener('click',selectFlavor,false);
+	}
+
+	function selectFilling(e){
+
+		var fillings=document.getElementById('Filling').getElementsByTagName('td');
+
+		for(var i = 0, len=fillings.length;i < len; i++) {
+			fillings[i].setAttribute('class','');
+		}
+
+		var selectedFilling = e.target;
+		selectedFilling.setAttribute('class','selected');
+	}
+	var fillings = document.getElementById('Filling').getElementsByTagName('td');
+
+	for(var i = 0, len = fillings.length;i<len;i++) {
+		fillings[i].addEventListener('click',selectFilling,false);
+	}
+
+	function selectIcing(e){
+
+		var icings=document.getElementById('Icing').getElementsByTagName('td');
+
+		for(var i = 0, len=icings.length;i < len; i++) {
+			icings[i].setAttribute('class','');
+		}
+
+		var selectedIcing = e.target;
+		selectedIcing.setAttribute('class','selected');
+	}
+	var icings = document.getElementById('Icing').getElementsByTagName('td');
+
+	for(var i = 0, len = icings.length;i<len;i++) {
+		icings[i].addEventListener('click',selectIcing,false);
+	}
+
+	function checkAllBoxes(){
+		var toppings = new Array();
+		toppings  = document.getElementById('toppings').getElementsByTagName('input');
+		for (var i = 0; i < toppings.length; i++)
+		{
+			if (toppings[i].type == 'checkbox')
+			{
+				toppings[i].checked = true;
+			}
+		}
+	}
+
+	function uncheckAllBoxes(){
+		var toppings = new Array();
+		toppings  = document.getElementById('toppings').getElementsByTagName('input');
+		for (var i = 0; i < toppings.length; i++)
+		{
+			if (toppings[i].type == 'checkbox')
+			{
+				toppings[i].checked = false;
+			}
+		}
+
 	}
 });
