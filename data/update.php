8ed385839d0298a6fedae5241c1acc6b8702de4f
picoriley <acloudy@mail.smu.edu>
2013-10-10 09:21:51 -0500

    update.php now can read a json file and print out the needed values for an SQL statement

--- data/update.php
+++ data/update.php
@@ -1,24 +1,28 @@
 <?php
+importDataToDB();
 function importDataToDB()
 {
-  $con = mysql_connect("localhost", "cupcaker", "nomnomnom") or die ("Could not connect: " . mysql_error());
-  mysql_select_db("customcupcakes", $con) or die ("Could not connect: " . mysql_error());
+  echo("<html><body>");
+  /*$con = mysql_connect("localhost", "cupcaker", "nomnomnom") or die ("Could not connect: " . mysql_error());
+  mysql_select_db("customcupcakes", $con) or die ("Could not connect: " . mysql_error());*/
 
   $json_string = 'http://54.200.82.84/Kustom-Kupcake/data/menu.json';
   $json = file_get_contents($json_string);
   
   $result = json_decode($json, TRUE);
-  /*foreach($result as $value) 
+  $i = 0;
+  //foreach($result as $value) 
   {
-      if($value) 
-      {
 	  //mysql_query
-	  echo("INSERT INTO cupcakes (flavorID, ypos, template) VALUES ($value['menu']['cakes']['flavor'], $value->ypos,$value->template)");
-      }
-  mysql_close($con);
-  }*/
-  include 'simplexlsx.class.php';
+	  echo("INSERT INTO flavor (flavorID, flavorName, picLoc) VALUES (".$i."," 
+	  .$result['menu']['cakes'][$i]['flavor']."," 
+	  .$result['menu']['cakes'][$i]['img_url'].")");
+	  $i++;
+  //mysql_close($con);
+  }
+  /*include 'simplexlsx.class.php';
   $xlsx = new SimpleXLSX('http://54.200.82.84/Kustom-Kupcake/data/CustomCupcakesDBData-FavoriteCupcakes.xlsx');
-  print_r( $xlsx->rows() );
+  echo( $xlsx->rows() );*/
+  echo("</html></body>");
 }
 ?>
