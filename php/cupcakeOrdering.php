45ec3560446b04ede2f889542a7d37b30a033807
MissingNo <spacepirate92@gmail.com>
2013-10-10 09:47:22 -0500

    Added PHP for reading tables

--- php/cupcakeOrdering.php
+++ php/cupcakeOrdering.php
@@ -10,7 +10,38 @@
 				die('Could not connect: ' . mysql_error());
 			}
 
-			mysql_select_db("")
+			mysql_select_db("customcupcakes");
+			$result = mysql_query("SELECT * FROM flavors");
+
+			echo "<table id="flavor">
+					<tbody>"
+			while($var = mysql_fetch_assoc($result)){
+				$pic = $var["picLock"];
+				$flavor = $var["flavorName"];
+			echo "<td> <img src="$pic"> $flavor</td>";}
+			echo "</tbody>
+				</table>";
+
+
+			$result = mysql_query("SELECT * FROM fillings");
+			echo "<table id="filling">
+					<tbody>";
+			while($var = mysql_fetch_assoc($result)){
+				$pic = $var["picLock"];
+				$filling = $var["fillingName"];
+			echo "<td> <img src="$pic"> $filling</td>";}
+			echo "</tbody>
+				</table>";
+
+			$result = mysql_query("SELECT * FROM icings");
+			echo "<table id="icing">
+					<tbody>";
+			while($var = mysql_fetch_assoc($result)){
+				$pic = $var["picLock"];
+				$icing = $var["icinggName"];
+			echo "<td> <img src="$pic"> $icing</td>";}
+			echo "</tbody>
+				</table>";
 		?>
 	<body>
 </html>
