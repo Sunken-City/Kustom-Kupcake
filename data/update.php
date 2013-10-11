fc8a37c42fc038747f9ae872415631e75362823f
picoriley <acloudy@mail.smu.edu>
2013-10-11 14:57:34 -0500

    Completed update.php, json is properly read into the database.

--- data/update.php
+++ data/update.php
@@ -10,6 +10,7 @@ function importDataToDB()
   $json = file_get_contents($json_string);
   
   $result = json_decode($json, TRUE);
+  
   $i = 0;
   $cake = $result['menu']['cakes'];
   $frosting = $result['menu']['frosting'];
@@ -17,45 +18,38 @@ function importDataToDB()
   $toppings = $result['menu']['Toppings'];
   foreach($cake as $value) 
   {
-	  //mysql_query
-	  echo("INSERT INTO flavor (flavorID, flavorName, picLoc) VALUES (".$i.",\"" 
+	  mysql_query("INSERT INTO flavor (flavorID, flavorName, picLoc) VALUES (".$i.",\"" 
 	  .$value['flavor']."\",\"" 
-	  .$value['img_url']."\");"."</br>");
+	  .$value['img_url']."\");");
 	  $i = $i + 1;
-  //mysql_close($con);
   }
   $i = 0;
   foreach($frosting as $value) 
   {
-	  //mysql_query
-	  echo("INSERT INTO icing (icingID, icingName, picLoc) VALUES (".$i.",\"" 
+	  mysql_query("INSERT INTO icing (icingID, icingName, picLoc) VALUES (".$i.",\"" 
 	  .$value['flavor']."\",\"" 
-	  .$value['img_url']."\");"."</br>");
+	  .$value['img_url']."\");");
 	  $i = $i + 1;
-  //mysql_close($con);
   }
   $i = 0;
   foreach($fillings as $value) 
   {
-	  //mysql_query
-	  echo("INSERT INTO filling (fillingID, fillingName, rgb) VALUES (".$i.",\"" 
+	  mysql_query("INSERT INTO filling (fillingID, fillingName, rgbVal) VALUES (".$i.",\"" 
 	  .$value['flavor']."\",\"" 
-	  .$value['rgb']."\");"."</br>");
+	  .$value['rgb']."\");");
 	  $i = $i + 1;
-  //mysql_close($con);
   }
   $i = 0;
   foreach($toppings as $value) 
   {
-	  //mysql_query
-	  echo("INSERT INTO toppings (toppingID, toppingName) VALUES (".$i.",\"" 
-	  .$value."\");"."</br>");
+	  mysql_query("INSERT INTO topping (toppingID, toppingName) VALUES (".$i.",\"" 
+	  .$value."\");");
 	  $i = $i + 1;
-  //mysql_close($con);
   }
+  mysql_close($con);
   /*include 'simplexlsx.class.php';
   $xlsx = new SimpleXLSX('http://54.200.82.84/Kustom-Kupcake/data/CustomCupcakesDBData-FavoriteCupcakes.xlsx');
   echo( $xlsx->rows() );*/
-  echo("</html></body>");
+  echo("Success!</body></html>");
 }
 ?>
