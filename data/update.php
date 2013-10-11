0e208788109fa5c773535a58f0e06d1ac8bfd5f6
picoriley <acloudy@mail.smu.edu>
2013-10-11 14:05:54 -0500

    Beginning work on the foreach loop for the json

--- data/update.php
+++ data/update.php
@@ -3,8 +3,8 @@ importDataToDB();
 function importDataToDB()
 {
   echo("<html><body>");
-  /*$con = mysql_connect("localhost", "cupcaker", "nomnomnom") or die ("Could not connect: " . mysql_error());
-  mysql_select_db("customcupcakes", $con) or die ("Could not connect: " . mysql_error());*/
+  $con = mysql_connect("localhost", "cupcaker", "nomnomnom") or die ("Could not connect: " . mysql_error());
+  mysql_select_db("customcupcakes", $con) or die ("Could not connect: " . mysql_error());
 
   $json_string = 'http://54.200.82.84/Kustom-Kupcake/data/menu.json';
   $json = file_get_contents($json_string);
