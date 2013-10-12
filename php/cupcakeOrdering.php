6a3d31c2c183a344d8806c26ea0d66e140b76f6c
nathanDmoore <moorednathan@gmail.com>
2013-10-11 22:58:23 -0500

    bloody

--- php/cupcakeOrdering.php
+++ php/cupcakeOrdering.php
@@ -29,7 +29,7 @@
 	$id = $id + 1;
 
 	$insertAllQuery = "INSERT INTO purchases (purchaseID,quantity,cupcakeID,fillingID,icingID,userID)
-			 VALUES ('123', '1', '1', '1', '1','121')";
+			 VALUES (123, 1, 1, 1, 1,123)";
 	mysqli_query($db,$insertAllQuery);
 
 	if (!mysqli_query($db,$insertAllQuery)) {
